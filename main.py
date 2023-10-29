2cf7e51022b88aed8c4d342e87d505c6f5996576
khali <klpham04@gmail.com>
2023-10-29 17:04:01 +0100

    working login
    username: admin
    password: admin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox  # Added for message box
 
 class App:
     def __init__(self):
@@ -27,7 +28,6 @@ class App:
         for frame in self.frames.values():
             frame.pack_forget()
         self.frames[frame_name].pack(fill='both', expand=True)
-        
 
 class LoginPage:
     def __init__(self, root, switch_frame):
@@ -52,17 +52,30 @@ class LoginPage:
         self.password_text_field.grid(row=2, column=1, padx=10, sticky='e')
 
         # Login Button
-        login_button = tk.Button(self.mainframe, text='Login', background='#a087c7', border=0, foreground='white', command=lambda: switch_frame("Home"))
+        login_button = tk.Button(self.mainframe, text='Login', background='#a087c7', border=0, foreground='white', command=lambda: self.check_credentials(switch_frame))
         login_button.grid(row=3, column=0, pady=20, padx=20, sticky='nswe')
+
+    def check_credentials(self, switch_frame):
+        username = self.username_text_field.get()
+        password = self.password_text_field.get()
+        if username == "admin" and password == "admin":
+            switch_frame("Home")
+        else:
+            messagebox.showerror("Login Error", "Invalid username or password")
+        self.clear_fields()
+            
+    def clear_fields(self):
+        self.username_text_field.delete(0, 'end')
+        self.password_text_field.delete(0, 'end')
 
 class HomePage:
     def __init__(self, root, switch_frame):
         self.mainframe = tk.Frame(root, bg='#333333')
-        self.mainframe.pack(fill='both', expand=True)
+        self.mainframe.pack(expand=True)
 
         # Home Page Label
-        home_label = ttk.Label(self.mainframe, text='Welcome to the Home Page', background='#333333', foreground='white', font=("Microsoft YaHei UI Light", 14))
-        home_label.grid(row=0, column=0, padx=20, pady=20)
+        home_label = ttk.Label(self.mainframe, text='Medibot Home', background='#333333', foreground='white', font=("Microsoft YaHei UI Light", 14))
+        home_label.grid(row=0, column=0, pady=20)
 
         # Back to Login Button
         back_button = tk.Button(self.mainframe, text='Back to Login', background='#a087c7', border=0, foreground='white', command=lambda: switch_frame("Login"))
