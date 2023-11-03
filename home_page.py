5900e3519a8f2ef5c0f2e88129ecd6e234fa9699
khali <klpham04@gmail.com>
2023-11-03 10:35:58 +0100

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -6,7 +6,6 @@ class HomePage:
         self.root = root
         self.mainframe = tk.Frame(self.root, bg='#333333')
         self.mainframe.pack(expand=True, fill='both')
-        self.mainframe.columnconfigure(0, weight=1)
 
         # Navigation Bar-------------------------------------------------------------------------- 
         navbar_frame = tk.Frame(self.mainframe, height=150, background='#a0a9de')
@@ -23,22 +22,28 @@ class HomePage:
             button.bind("<Leave>", self.on_button_leave)  # leave event
 
         # Home Page Content-------------------------------------------------------------------------- 
-        home_label = ttk.Label(self.mainframe, text='Medibot Home', background='#333333', foreground='white', font=("Microsoft YaHei UI Light", 14))
-        home_label.pack(pady=20)
+        left_frame = tk.Frame(self.mainframe, background='red', width=300)
+        left_frame.pack(fill='y', side='left', pady=20, padx=30)
+        
+        right_frame = tk.Frame(self.mainframe, background='blue', width=400)
+        right_frame.pack(fill='y', side='right', pady=20, padx=30)
+        
+        home_label = ttk.Label(left_frame, text='Medibot Home', background='#333333', foreground='white', font=("Microsoft YaHei UI Light", 45))
+        home_label.pack(pady=20, fill='both', anchor='center',expand=True)
 
-        placeholder_label = ttk.Label(self.mainframe, text='Welcome to Medibot. This is the home page.', background='#333333', foreground='white', font=("Microsoft YaHei UI Light", 12))
-        placeholder_label.pack(pady=20)
+        status_label = ttk.Label(left_frame, text='current Status: connected', background='#333333', foreground='white', font=("Microsoft YaHei UI Light", 18))
+        status_label.pack(pady=20)
 
     def button_click(self, button_text, switch_frame):
         if button_text == 'Logout':
             # Logout Firebase
             switch_frame('Login')
         elif button_text == 'Send Robot':
+            switch_frame('Send Robot')
             pass    
 
     def on_button_hover(self, event):
         event.widget['background'] = '#8c94c6' 
 
     def on_button_leave(self, event):
-        event.widget['background'] = '#a0a9de' 
-
+        event.widget['background'] = '#a0a9de'
