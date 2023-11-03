26a0f7af8c51ebfb1bd15c28abe0f6e09e8198eb
khali <klpham04@gmail.com>
2023-11-03 09:42:42 +0100

    working navbar

--- home_page.py
+++ home_page.py
@@ -9,15 +9,18 @@ class HomePage:
         self.mainframe.columnconfigure(0, weight=1)
 
         # Navigation Bar-------------------------------------------------------------------------- 
-        navbar_frame = tk.Frame(self.mainframe, height=100, background='#a0a9de')
+        navbar_frame = tk.Frame(self.mainframe, height=150, background='#a0a9de')
         navbar_frame.pack(fill='x')
 
         buttons = ['Home', 'Send Robot', 'Feedback', 'Scan Area', 'Logout']
         self.nav_buttons = []
         for button_text in buttons:
-            button = tk.Button(navbar_frame, text=button_text, command=lambda text=button_text: self.button_click(text),bg='#a0a9de')
+            button = tk.Button(navbar_frame, text=button_text, command=lambda text=button_text: self.button_click(text), bg='#a0a9de', bd=0)
+            button.config(height=3) 
             button.pack(side='left', fill='both', expand=True)
             self.nav_buttons.append(button)
+            button.bind("<Enter>", self.on_button_hover)  # hover event
+            button.bind("<Leave>", self.on_button_leave)  # leave event
 
         # Home Page Content-------------------------------------------------------------------------- 
         home_label = ttk.Label(self.mainframe, text='Medibot Home', background='#333333', foreground='white', font=("Microsoft YaHei UI Light", 14))
@@ -26,7 +29,14 @@ class HomePage:
         placeholder_label = ttk.Label(self.mainframe, text='Welcome to Medibot. This is the home page.', background='#333333', foreground='white', font=("Microsoft YaHei UI Light", 12))
         placeholder_label.pack(pady=20)
 
-    def button_click(self, button_text):
+    def button_click(self, button_text, switch_frame):
         if button_text == 'Logout':
             # Implement the functionality to log out here
             pass
+
+    def on_button_hover(self, event):
+        event.widget['background'] = '#8c94c6' 
+
+    def on_button_leave(self, event):
+        event.widget['background'] = '#a0a9de' 
+
