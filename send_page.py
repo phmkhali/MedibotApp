0de0525a44aabf939b2c0537665aa3f0670f8952
Julius Jeske <s_jeskej21@stud.hwr-berlin.de>
2023-11-03 09:51:22 +0100

    created Send_Page

---
+++ send_page.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+class sendRobotPage:
+    def __init__(self, root, switch_frame):
+        self.root = root
+        self.mainframe = tk.Frame(self.root, bg='#333333')
+        self.mainframe.pack(expand=True, fill='both')
+        self.mainframe.columnconfigure(0, weight=1)
+
+        # Navigation Bar-------------------------------------------------------------------------- 
+        navbar_frame = tk.Frame(self.mainframe, height=150, background='#a0a9de')
+        navbar_frame.pack(fill='x')
+
+        buttons = ['Home', 'Send Robot', 'Feedback', 'Scan Area', 'Logout']
+        self.nav_buttons = []
+        for button_text in buttons:
+            button = tk.Button(navbar_frame, text=button_text, command=lambda text=button_text: self.button_click(text), bg='#a0a9de', bd=0)
+            button.config(height=3) 
+            button.pack(side='left', fill='both', expand=True)
+            self.nav_buttons.append(button)
+            button.bind("<Enter>", self.on_button_hover)  # hover event
+            button.bind("<Leave>", self.on_button_leave)  # leave event    
