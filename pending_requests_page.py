7dccc858b2b9e10386aec2494689d12ad7b0950e
Julius Jeske <s_jeskej21@stud.hwr-berlin.de>
2023-11-12 15:21:17 +0100

    added the submit button

--- pending_requests_page.py
+++ pending_requests_page.py
@@ -6,3 +6,11 @@ class PendingRequestsPage:
         self.root = root
         self.mainframe = tk.Frame(self.root, bg='#333333')
         self.mainframe.pack(expand=True, fill='both')
+
+
+
+submit_button = ttk.Button(self.main_frame, text="Submit", command=submit)
+submit_button.grid(row=0, column=2, padx=5, pady=5, sticky=tk.W)  
+
+def submit():
+    pass
