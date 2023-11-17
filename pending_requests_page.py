ea0e7118ca019f603a9c1ca8eddb2cc3c7de4bed
khali <klpham04@gmail.com>
2023-11-17 10:32:41 +0100

    nav bar change

--- pending_requests_page.py
+++ pending_requests_page.py
@@ -32,36 +32,36 @@ class PendingRequestsPage:
                 
         # Page Content--------------------------------------------------------------------------
         self.placeholder_frame = tk.Frame(self.mainframe, width=400, background='#333333')
-        self.placeholder_frame.pack(side='right', padx=30, pady= 20, fill='y')
+        self.placeholder_frame.pack(side='right', padx=30, pady= 20, fill='both')
 
         self.left_frame = tk.Frame(self.mainframe, background='green')
         self.left_frame.pack(fill='both', side='left', pady=20, padx=30, expand=True)
         
         self.right_frame = tk.Frame(self.placeholder_frame)
-        self.right_frame.pack(fill='both', expand=True)
+        self.right_frame.pack(fill='both')
         self.right_frame.pack_forget()
 
         requestsList = tk.Listbox(self.left_frame)
-        #requestsList.grid( row= 0, column= 0, padx=20 , pady= 20 , fill= tk.BOTH)
-        requestsList.pack(fill = tk.BOTH, expand= True)
+        requestsList.pack(fill ='both', expand= True)
         requestsList.bind("<<ListboxSelect>>", self.on_select)
+        
         # todo: Liste sinnvoll befüllen
         requestsList.insert(tk.END, "Paracetamol 500            2 Qty       Room 3") #zum Testen
         requestsList.insert(tk.END, "Paracetamol 500            1 Qty       Room 2")
         requestsList.insert(tk.END, "Calcium Sandoz 500         1 Qty       Room 2")
 
-        self.Info_label = ttk.Label(self.right_frame , text="Here should be all the Information")
-        self.Info_label.grid(row= 0, column=2, padx= 5, pady= 5, sticky=tk.N)
+        self.info_label = ttk.Label(self.right_frame , text="Here should be all the Information")
+        self.info_label.pack()
         
         self.right_frame.rowconfigure(1, weight=1)
-        submit_button = ttk.Button(self.right_frame, text="Submit", command=lambda text=button_text: self.button_click(text, switch_frame) )
-        submit_button.grid(row=5, column=2, padx=5, pady=5, sticky=tk.S)  
+        submit_button = tk.Button(self.right_frame,  relief='flat', background='#4C4273', foreground='white', width='12', text="Submit", command=lambda text=button_text: self.button_click(text, switch_frame) )
+        submit_button.pack(side='bottom',pady=10)  
 
 
     def on_select(self, event):
         selected_item = event.widget.get(event.widget.curselection())
         #Je nachdem was in der Liste ist hier anpassen!
-        self.Info_label["text"] = selected_item
+        self.info_label["text"] = selected_item
         self.right_frame.pack(fill='both', side='right', pady=20, padx=30)
 
     def button_click(self, button_text, switch_frame):
