560d79f319a617adcd727d62cfba2be3a0db6c8a
Julius Jeske <s_jeskej21@stud.hwr-berlin.de>
2023-11-24 15:08:50 +0100

    added patientName

--- request.py
+++ request.py
@@ -1,8 +1,9 @@
 class Request:
-    def __init__(self, medName, location, quantity, user, status):
+    def __init__(self, medName, location, quantity, patientName, user, status):
         self.med_name = medName 
         self.location = location
         self.quantity = quantity
+        self.patientName = patientName
         self.user = user
         self.status = status
  
