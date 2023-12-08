92ff5b0d204efa451cd9c1f419d3321f3c41ea6f
Julius Jeske <s_jeskej21@stud.hwr-berlin.de>
2023-12-08 15:06:31 +0100

    fire_id added

--- request.py
+++ request.py
@@ -1,11 +1,12 @@
 class Request:
-    def __init__(self, medName, location, quantity, patientName, user, status):
+    def __init__(self, medName, location, quantity, patientName, user, status, fire_id):
         self.med_name = medName 
         self.location = location
         self.quantity = quantity
         self.patientName = patientName
         self.user = user
         self.status = status
+        self.fire_id = fire_id
  
 
     def set_status_processing(self):
